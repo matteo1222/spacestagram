270a4b15f946a54783cb0d8537a60f3296bb313d
Matthew Lu <seedmachinegun@gmail.com>
2022-08-03 00:40:25 +0100

    Refactor with URLSearchParams in fetchPictures

--- src/api/fetchPictures.ts
+++ src/api/fetchPictures.ts
@@ -8,7 +8,15 @@ const BASE_URL = 'https://api.nasa.gov/planetary/apod'
 // TODO: write a Netlify function to make NASA API request to hide the API Key
 async function checkIfTodayHasPicture() {
   const todayDate = format(new Date(), 'yyyy-MM-dd')
-  const response = await fetch(`${BASE_URL}?api_key=${API_KEY}&date=${todayDate}`)
+
+  const searchParams = new URLSearchParams();
+
+  if (API_KEY) {
+    searchParams.append('api_key', API_KEY)
+  }
+  searchParams.append('date', todayDate)
+
+  const response = await fetch(`${BASE_URL}?${searchParams.toString()}`)
   if (response.ok) {
     return true
   }
@@ -22,9 +30,20 @@ export async function fetchPictures(context: QueryFunctionContext) {
   if (context.pageParam === 0) {
     subtractDays = await checkIfTodayHasPicture() ? 0 : 1
   }
+  // startDate is the earliest date
   const startDate = format(addDays(new Date(), -BATCH_FETCH_DAYS * (context.pageParam + 1) + 1), 'yyyy-MM-dd')
+  // endDate is the latest date
   const endDate = format(addDays(new Date(), -BATCH_FETCH_DAYS * context.pageParam - subtractDays), 'yyyy-MM-dd')
-  const response = await fetch(`${BASE_URL}?api_key=${API_KEY}&start_date=${startDate}&end_date=${endDate}`)
+
+  const searchParams = new URLSearchParams();
+
+  if (API_KEY) {
+    searchParams.append('api_key', API_KEY)
+  }
+  searchParams.append('start_date', startDate)
+  searchParams.append('end_date', endDate)
+
+  const response = await fetch(`${BASE_URL}?${searchParams.toString()}`)
   if (!response.ok) {
     throw new Error('Network request does not success')
   }
